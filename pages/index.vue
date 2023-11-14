d8d50970270cf0f53d5ce9ddbc319f1fc1e0b7d4
elonehoo <hi@elonehoo.me>
2023-11-14 21:38:33 +0800

    fix: added theme scheme for Splitpanes component

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <Splitpanes style="height: 400px">
-    <SplitpanesPane min-size="20">1</SplitpanesPane>
-  <SplitpanesPane>
-    <splitpanes horizontal>
-      <SplitpanesPane>2</SplitpanesPane>
-      <SplitpanesPane>3</SplitpanesPane>
-      <SplitpanesPane>4</SplitpanesPane>
-    </splitpanes>
-  </SplitpanesPane>
-  <SplitpanesPane>5</SplitpanesPane>
+  <Splitpanes class="default-theme" style="height: 400px">
+    <SplitpanesPane min-size="20">
+1
+</SplitpanesPane>
+    <SplitpanesPane>
+      <splitpanes horizontal>
+        <SplitpanesPane>2</SplitpanesPane>
+        <SplitpanesPane>3</SplitpanesPane>
+        <SplitpanesPane>4</SplitpanesPane>
+      </splitpanes>
+    </SplitpanesPane>
+    <SplitpanesPane>5</SplitpanesPane>
   </Splitpanes>
 </template>
 
