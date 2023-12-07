bc9f1fa67788e76c6a19394381ce9f7c7188067c
elonehoo <hi@elonehoo.me>
2023-12-07 09:55:25 +0800

    fix: remove function

--- components/layout/Root.vue
+++ components/layout/Root.vue
@@ -2,17 +2,11 @@
 defineOptions({
   name: 'LayoutRoot',
 })
-
-const sidebar = useSidebarStore()
-
-function splitpanesResize(size: number) {
-  sidebar.panSize.size = size
-}
 </script>
 
 <template>
   <LayoutBasic :border-positions="[]">
-    <Splitpanes @resize="splitpanesResize($event[0].size)">
+    <Splitpanes>
       <Sidebar />
       <SplitpanesPane :size="80" :min-size="70" :max-size="85">
         <slot />
