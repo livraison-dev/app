acb28be7793868e7985c9c3b7752bd93dc656ab1
elonehoo <hi@elonehoo.me>
2023-12-22 09:53:56 +0800

    fix: problem that produces a sidebar that cannot be displayed in some browsers

--- components/layout/Root.vue
+++ components/layout/Root.vue
@@ -2,11 +2,15 @@
 defineOptions({
   name: 'LayoutRoot',
 })
+
+const sidebar = useSidebarStore()
+
+const { panSize } = storeToRefs(sidebar)
 </script>
 
 <template>
   <LayoutBasic :border-positions="[]">
-    <Splitpanes>
+    <Splitpanes @resize="panSize.size = $event[0].size">
       <Sidebar />
       <SplitpanesPane :size="80" :min-size="70" :max-size="85">
         <slot />
