4bf1b9b4291142ff608ee7d9cc3ae7a46809aa2c
elonehoo <hi@elonehoo.me>
2024-01-01 17:17:14 +0800

    fix: update ui input component

--- ui/input/Input.vue
+++ ui/input/Input.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
+import { useVModel } from '@vueuse/core'
+
 defineOptions({
   name: 'LivraisonInput',
 })
 
-const url = defineModel<string>()
+const props = withDefaults(
+  defineProps<{
+    modelValue?: string | number
+    icon?: string
+    placeholder?: string
+    disabled?: boolean
+    autofocus?: boolean
+    readonly?: boolean
+    type?: string
+  }>(),
+  {
+    modelValue: '',
+    type: 'text',
+  },
+)
+
+const emit = defineEmits<{ (...args: any): void }>()
+const input = useVModel(props, 'modelValue', emit, { passive: true })
 </script>
 
 <template>
-  <input v-model="url" type="text">
+  <div class="n-text-input flex flex items-center border app-border-base rounded py-1 pl-1 pr-2 focus-within:app-focus-base focus-within:border-context app-bg-base">
+    <slot name="icon">
+      <NIcon v-if="icon" :icon="icon" class="ml-0.3em mr-0.1em text-1.1em op50" />
+    </slot>
+    <input
+      v-model="input"
+      v-bind="$props as any"
+      class="ml-0.4em w-full flex-auto app-bg-base !outline-none"
+    >
+  </div>
 </template>
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,10 @@
+import type { RuleContext } from '@unocss/core'
+import type { Theme } from '@unocss/preset-uno'
+import { parseColor } from '@unocss/preset-mini/utils'
+import { theme as unoTheme } from '@unocss/preset-mini'
 import {
   defineConfig,
+  mergeDeep,
   presetAttributify,
   presetIcons,
   presetTypography,
@@ -10,6 +15,21 @@ import {
 } from 'unocss'
 
 export default defineConfig({
+  theme: mergeDeep<Theme>(unoTheme, {
+    colors: {
+      context: 'rgba(var(--nui-c-context),%alpha)',
+    },
+  }),
+  rules: [
+    [/^n-(.*)$/, ([, body]: string[], { theme }: RuleContext<Theme>) => {
+      const color = parseColor(body, theme)
+      if (color?.cssColor?.type === 'rgb' && color.cssColor.components) {
+        return {
+          '--nui-c-context': `${color.cssColor.components.join(',')}`,
+        }
+      }
+    }],
+  ],
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
@@ -18,6 +38,9 @@ export default defineConfig({
     ['PUT', 'inline-block text-cyan-400'],
     ['DELETE', 'inline-block text-rose-500'],
     ['icon-hover', 'hover:bg-black hover:bg-op-4 dark:hover:bg-white dark:hover:bg-op-10 rounded'],
+    ['app-bg-base', 'bg-white dark:bg-[#151515]'],
+    ['app-border-base', 'border-gray/20'],
+    ['app-focus-base', 'ring-2 ring-context/50'],
   ],
   presets: [
     presetUno(),
